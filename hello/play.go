b6a861c21c26aba1e98411b6475e4a355a66857d
az-z <az-z@users.noreply.github.com>
2014-12-26 22:46:55 -0500

    some changes

--- hello/play.go
+++ hello/play.go
@@ -2,25 +2,43 @@ package main
 
 import (
 	"fmt"
-	"time"
+	//	"time"
 )
 
+func f_test(i int) int {
+	a := i
+
+}
 func main() {
-	fmt.Println("When's Saturday?")
-	today := time.Now().Weekday()
-	switch time.Saturday {
-	case today + 0:
-		fmt.Println("Today.")
-	case today + 1:
-		fmt.Println("Tomorrow.")
-	case today + 2:
-		fmt.Println("In two days.")
-	default:
-		fmt.Println("Too far away.")
+
+	a := []int{10, 20, 30}
+	b := []int{1, 2, 3}
+	c := append(a, b...)
+	fmt.Println("c - ", c)
+	f_test(100)
+	i = 2
+	x = []int{3, 5, 7}
+	for i, x[i] = range x {
+		// set i, x[2] = 0, x[0]
+		fmt.Println("i - ", i)
+		fmt.Println("x[i] - ", x[i])
 	}
 
-	for i := 0; i < 3; {
-		fmt.Println("i - ", i)
-		i = i + 1
-	}
+	fmt.Println("When's Saturday?")
+	// today := time.Now().Weekday()
+	// switch time.Saturday {
+	// case today + 0:
+	// 	fmt.Println("Today.")
+	// case today + 1:
+	// 	fmt.Println("Tomorrow.")
+	// case today + 2:
+	// 	fmt.Println("In two days.")
+	// default:
+	// 	fmt.Println("Too far away.")
+	// }
+
+	// for i := 0; i < 3; {
+	// 	fmt.Println("i - ", i)
+	// 	i = i + 1
+	// }
 }

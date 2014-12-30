12e391428124e5906a2f9501a2f40d26f08c1b15
az-z <az-z@users.noreply.github.com>
2014-12-30 17:18:20 -0500

    working version for sort string

--- hello/Pic.go
+++ hello/Pic.go
@@ -1,12 +1,38 @@
 package main
 
 import (
-	"code.google.com/p/go-tour/pic"
-	"code.google.com/p/go-tour/wc"
+	// "code.google.com/p/go-tour/pic"
+	// "code.google.com/p/go-tour/wc"
+	"fmt"
+	//"sort"
+	st "strings"
 )
 
-func WordCount(s string) map[string]int {
-	return map[string]int{"x": 1}
+var (
+	v_str = "a a a a big and fat fat brown fox been brown"
+)
+
+func WordCount(s []string) map[string]int {
+	var (
+	//	exist bool //= true
+	)
+	sorted_map := make(map[string]int)
+
+	for i := 0; i < len(s); i++ {
+		if _, exist := sorted_map[s[i]]; exist {
+			sorted_map[s[i]] = sorted_map[s[i]] + 1
+			//fmt.Println("sorted_map[i] - ", sorted_map[s[i]])
+		} else {
+			sorted_map[s[i]] = 1
+		}
+	}
+
+	return sorted_map
+	//map[string]int{"x": 1}
+}
+
+func split_string(str string) {
+	fmt.Println(st.Fields(str))
 }
 
 func Pic(dx, dy int) [][]uint8 {
@@ -18,7 +44,13 @@ func Pic(dx, dy int) [][]uint8 {
 }
 
 func main() {
-	pic.Show(Pic)
-	wc.Test(WordCount)
+	// pic.Show(Pic)
+	slice_s := st.Fields(v_str)
+	//sorted_slice := sort.Sort(slice_s)
+
+	//WordCount(slice_s)
+	fmt.Printf("slice_s - %v \n", slice_s)
+	fmt.Printf("sorted_map - %v", WordCount(slice_s))
+	// wc.Test(WordCount)
 
 }

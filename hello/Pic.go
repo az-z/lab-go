6388a823a95b4ecc23b88d3cdb21afd365eb5c4a
az-z <az-z@users.noreply.github.com>
2015-01-11 18:20:44 -0500

    working version

--- hello/Pic.go
+++ hello/Pic.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	reg "regexp"
 	// "code.google.com/p/go-tour/pic"
 	// "code.google.com/p/go-tour/wc"
 	"fmt"
@@ -11,41 +12,36 @@ import (
 )
 
 var (
-	v_str             = "a a a a big and fat fat brown fox been brown"
+	v_str             = "ab ab ab ab, big and fat fat brown fox been brown"
 	slice_s, slice_s1 []string
 )
 
-func WordCount(s []string) map[string]int {
+func WordCount(s []string) (map[string]int, string) {
 	var (
-	// ind int
+		max_key   string
+		max_value int = 0
 	)
 	sorted_map := make(map[string]int)
 
-	for i := 0; i < len(s); i++ {
+	for i := range s {
 		if _, exist := sorted_map[s[i]]; exist {
 			sorted_map[s[i]] = sorted_map[s[i]] + 1
-			// fmt.Printf("sorted_map[%v] - %v \n", i, sorted_map[s[i]])
+			// fmt.Printf("%v - %v\n", sorted_map[s[i]], s[i])
+			if sorted_map[s[i]] > max_value {
+				// fmt.Printf("max_value - %v, max_key - %v \n", max_value, max_key)
+				max_key = s[i]
+				max_value = sorted_map[s[i]]
+			} // fmt.Printf("sorted_map[%v] - %v \n", i, sorted_map[s[i]])
 		} else {
 			sorted_map[s[i]] = 1
 		}
 		// fmt.Println(sorted_map)
 	}
 
-	return sorted_map
+	return sorted_map, max_key
 	//map[string]int{"x": 1}
 }
 
-func split_string(str string) {
-	fmt.Println(st.Fields(str))
-}
-
-func Pic(dx, dy int) [][]uint8 {
-	slce := make([][]uint8, dx)
-	for y := 0; y < dy; y++ {
-		slce[y] = make([]uint8, dx)
-	}
-	return slce
-}
 func check(e error) {
 	if e != nil {
 		panic(e)
@@ -58,16 +54,31 @@ func readFile(s string) *string {
 	str_ := string(dat)
 	return &str_
 }
+func f_eval(incoming rune) bool {
+	compiled, _ := reg.Compile(`[[:alpha:]]`)
+	matched := compiled.MatchString(string(incoming))
+	// matched := compiled.MatchReader(incoming)
+	/*fmt.Printf("err - %v , incoming - %v , compiled -%v , matched - %v \n", err, string(rune(incoming)),
+	compiled, matched)
+	*/return !matched
+}
 
 func main() {
 	// pic.Show(Pic)
-	// v_str = *(readFile("/home/az/work/GO/data/10.txt.utf-8"))
+	v_str = *(readFile("/home/az/work/GO/data/10.txt.utf-8"))
 	// v_str = *(readFile("/home/az/work/GO/data/https://archive.org/stream/Scudry-ArtamneOuLeGrandCyrusTroisimePartie1654/Scudry-ArtamneOuLeGrandCyrusTroisimePartie1654_djvu.txt"))
-	slice_s = st.Fields(v_str)
-
+	slice_s = st.Fields(st.ToLower(v_str))
+	for i := range slice_s {
+		// fmt.Println(slice_s[i])
+		slice_s[i] = st.TrimFunc(slice_s[i], f_eval)
+	}
 	sort.Strings(sort.StringSlice(slice_s))
-	fmt.Printf("sorted slice_s - %v \n", slice_s)
-	fmt.Printf("sorted_map - %v", WordCount(slice_s))
+	// fmt.Printf("sorted slice_s - %v \n", slice_s)
+	// fmt.Printf("sorted_map - %v", WordCount(slice_s))
+	ordered_map, the_key := WordCount(slice_s)
+
+	fmt.Printf("The most frequently used word is \"%v\"- %v \n", the_key, ordered_map[the_key])
+	fmt.Println("total count of words - ", len(slice_s))
 	// wc.Test(WordCount)
 
 }
